b0f69757f7e898e6722bc6949053628a65e60001
aberic <100309595@qq.com>
2019-05-26 11:01:09 +0800

    测试 websocket

---
+++ examples/websocket/client1/client1.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"github.com/ennoo/rivet"
+	"github.com/ennoo/rivet/trans/response"
+	"github.com/ennoo/rivet/utils/log"
+	"golang.org/x/net/websocket"
+)
+
+func main() {
+	rivet.Initialize(false, false, false)
+	rivet.ListenAndServe(&rivet.ListenServe{
+		Engine:      rivet.SetupRouter(testRouter1),
+		DefaultPort: "8082",
+	})
+}
+
+func testRouter1(router *response.Router) {
+	// 仓库相关路由设置
+	router.Group = router.Engine.Group("/rivet")
+	router.GET("/get", get)
+	router.GET("/send", send)
+	router.GET("/close", close)
+}
+
+func get(router *response.Router) {
+	//context := router.Context
+	//context.Header("Upgrade", "websocket")
+	//rivet.Request().Call(router.Context, http.MethodGet, "http://localhost:8081", "rivet/ws")
+	connect()
+	sendAndReceive()
+}
+
+func send(router *response.Router) {
+	//context := router.Context
+	//context.Header("Upgrade", "websocket")
+	//rivet.Request().Call(router.Context, http.MethodGet, "http://localhost:8081", "rivet/ws")
+	sendAndReceive()
+}
+
+func close(router *response.Router) {
+	closeConn()
+}
+
+var (
+	origin = "http://localhost:8081/"
+	url    = "ws://localhost:8081/rivet/ws/young"
+	conn   *websocket.Conn
+	err    error
+)
+
+func connect() {
+	conn, err = websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Self.Warn("error", log.Error(err))
+	}
+	go receive()
+}
+
+func sendAndReceive() {
+	message := []byte("hello, world!你好")
+	_, err = conn.Write(message)
+	if err != nil {
+		log.Self.Warn("error", log.Error(err))
+	}
+	log.Self.Debug("Send", log.String("message", string(message)))
+}
+
+func closeConn() {
+	conn.Close() //关闭连接
+}
+
+func receive() {
+	for {
+		var msg = make([]byte, 512)
+		m, err := conn.Read(msg)
+		if err != nil {
+			log.Self.Warn("error", log.Error(err))
+		}
+		log.Self.Debug("Receive", log.String("message", string(msg[:m])))
+	}
+}
---
+++ keeps.go
@@ -0,0 +1,32 @@
+package rivet
+
+import (
+	"github.com/ennoo/rivet/keeps"
+	"github.com/ennoo/rivet/trans/response"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var (
+	upgrade = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+// WS websocket 启动
+func WS(router *response.Router) {
+	response.SyncPoolGetResponse().DoSelf(router.Context, func(writer http.ResponseWriter, request *http.Request) {
+		id := router.Context.Param("id")
+		var (
+			conn *websocket.Conn
+			err  error
+		)
+		if conn, err = upgrade.Upgrade(writer, request, nil); nil != err {
+			return
+		}
+		Keepers = append(Keepers, keeps.Start(id, conn))
+	})
+}
